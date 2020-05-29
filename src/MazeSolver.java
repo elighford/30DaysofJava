66aa111dd2dabd6e1004015be6733f032af7b12a
elighford <eligh.ford@gmail.com>
2020-05-29 11:33:47 -0700

    Basics of maze

---
+++ src/MazeSolver.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class MazeSolver {
+	
+	static int [][] maze = {
+			{2, 0, 1, 1},
+			{1, 1, 0, 1},
+			{0, 1, 1, 1}		
+	};
+	//0 = wall
+	//1= path
+	//2= destination
+	
+	static LinkedList<Position> path = new LinkedList<Position>();
+	public static void main(String[] args) {
+		Position p = new Position(3, 0);
+		p.x = 3;
+		p.y = 0;
+		
+		path.push(p);
+		
+		maze[path.peek().x][path.peek().y] =0;
+
+	}
+
+}
