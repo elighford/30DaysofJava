859332fb9f6347bca482adaf06b0eb3f755a0c54
elighford <eligh.ford@gmail.com>
2020-06-04 13:14:03 -0700

    Maze Solver

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -4,8 +4,8 @@ public class MazeSolver {
 	
 	static int [][] maze = {
 			{2, 0, 1, 1},
-			{1, 1, 0, 1},
-			{0, 1, 1, 1}		
+			{1, 1, 1, 0},
+			{0, 0, 0, 1}		
 	};
 	//0 = wall
 	//1= path
@@ -13,13 +13,64 @@ public class MazeSolver {
 	
 	static LinkedList<Position> path = new LinkedList<Position>();
 	public static void main(String[] args) {
-		Position p = new Position(3, 0);
+		Position p = new Position(0, 3);
 		p.x = 3;
 		p.y = 0;
 		
 		path.push(p);
 		
-		maze[path.peek().x][path.peek().y] =0;
+		
+		
+		
+		
+		while(true) {
+			// down
+			int y = path.peek().y;
+			int x = path.peek().x;
+			maze[y][x] =0;
+			
+			if(maze[y+1][x] ==2) {
+				System.out.println("Moved down. You won!");
+				return;
+			}else if(maze[y+1][x] ==1) {
+				System.out.println("Moved down");
+				path.push(new Position(y+1, x));
+				continue;
+				
+			}
+			// left
+			if(maze[y][x-1] ==2) {
+				System.out.println("Moved left. You won!");
+				return;
+			}else if(maze[y][x-1] ==1) {
+				System.out.println("Moved left");
+				path.push(new Position(y, x-1));
+				continue;
+				
+			}
+			// up
+			if(maze[y-1][x] ==2) {
+				System.out.println("Moved up. You won!");
+				return;
+			}else if(maze[y-1][x] ==1) {
+				System.out.println("Moved up");
+				path.push(new Position(y-1, x));
+				continue;
+				
+			}
+			// right 
+			if(maze[y][x+1] ==2) {
+				System.out.println("Moved right. You won!");
+				return;
+			}else if(maze[y][x+1] ==1) {
+				System.out.println("Moved right");
+				path.push(new Position(y, x+1));
+				continue;
+				
+			}
+		}
+		
+		
 
 	}
 
